67a218acd1e6cb68976a7ceff96ae1293d6e3952
Gabriel Brun <lydstyl@gmail.com>
2018-10-30 19:48:49 +0100

    scss and sass together :-)

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/style.scss')
+  return gulp.src('./sass/style.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('default', function () {
-    gulp.watch('./sass/**/*.scss', function () {
-        return gulp.src('./sass/style.scss')
+    gulp.watch('./sass/**/*.sass', function () {
+        return gulp.src('./sass/style.sass')
           .pipe(sass().on('error', sass.logError))
           .pipe(gulp.dest('./css/'));
       
